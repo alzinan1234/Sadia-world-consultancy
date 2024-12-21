1e4d849d3136445be5aecfb6f39c4ecfcae2e51d
Al-Zinan <pappurohan1100@gmail.com>
2024-12-21 20:52:20 +0600

    added visa prossing

---
+++ src/components/Home/country/VisaProcessing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const VisaProcessing = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <img className="md:w-[456px] md:h-[332px]" src="" alt="" />
+        </div>
+        <div>
+          <h2 className="md:text-[40px] text-[30px] font-semibold">
+            Why choose Sadia Word <br /> Consultancy for your visa <br />{" "}
+            processing needs?
+          </h2>
+          <p>
+            SWC is a trusted, tech-based visa service in Bangladesh, offering
+            secure and expert visa processing through an easy online platform.
+            Their reliable expertise ensures a smooth application process for
+            all.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VisaProcessing;
