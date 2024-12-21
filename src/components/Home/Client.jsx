9e4131b07f8c719598a573e241251baeaca3d497
Al-Zinan <pappurohan1100@gmail.com>
2024-12-22 01:18:42 +0600

    added

--- src/components/Home/Client.jsx
+++ src/components/Home/Client.jsx
@@ -8,7 +8,7 @@ import client4 from "../assets/client/Rectangle (5).png";
 const Client = () => {
   return (
     <>
-      <div className="mb-9">
+      <div className="mt-[59px] mb-4">
         <h1 className="text-[40px] font-semibold text-center">
           Our Happy Clients
         </h1>
@@ -17,12 +17,14 @@ const Client = () => {
           Consultancy to make their travel and study <br /> dreams come true!
         </p>
       </div>
-      <div className="maxWidth mx-auto mb-[12px]">
-        <div className=" md:flex gap-[13px]">
-          <Image src={client1} alt="" />
-          <Image src={client2} alt="" />
-          <Image src={client3} alt="" />
-          <Image src={client4} alt="" />
+      <div className="mb-[182px]">
+        <div className="maxWidth mx-auto ">
+          <div className=" md:flex gap-[13px]">
+            <Image src={client1} alt="" />
+            <Image src={client2} alt="" />
+            <Image src={client3} alt="" />
+            <Image src={client4} alt="" />
+          </div>
         </div>
       </div>
     </>
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,55 @@
+import Image from "next/image";
 import React from "react";
+import LogoFooter from "../assets/navbar/Logo 2.png";
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="bg-[#E8F9FD] text-black">
+      <div className="maxWidth mx-auto py-[122px] grid grid-cols-1 md:grid-cols-3 gap-[100px] justify-between">
+        {/* Logo Section */}
+        <div className="text-center md:text-left">
+          <Image src={LogoFooter} alt="" />
+        </div>
+
+        {/* Contact Section */}
+        <div>
+          <h2 className="font-bold text-[32px] mb-4">Contact Us</h2>
+          <ul className="space-y-5">
+            <li className="flex items-center">
+              <FaEnvelope className="mr-3 text-[#007EE5]" />
+              <span>sadiyawc20@gmail.com</span>
+            </li>
+            <li className="flex items-center">
+              <FaPhoneAlt className="mr-3 text-[#007EE5]" />
+              <span>+8801300-935303, +8801892-968785</span>
+            </li>
+            <li className="flex items-center">
+              <FaMapMarkerAlt className="mr-3 text-[#007EE5]" />
+              <span>
+                Mahbub Plaza, (6th Floor), Indira Road, Farmgate, Dhaka-1215.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        {/* Services Section */}
+        <div>
+          <h2 className="font-bold text-[32px] mb-4">Our Services</h2>
+          <ul className="space-y-3">
+            <li>Multiple Visit Visa</li>
+            <li>Student Visa</li>
+            <li>Air Ticket</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Bottom Footer */}
+      <div className="bg-gray-700 text-white text-center py-3">
+        <p>Design & Developed By Nexgen Innovators</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
