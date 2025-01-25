e6e3303c78b385037cec815eebb30ec3415b1910
Al-Zinan <108150533+johir-dev@users.noreply.github.com>
2025-01-25 17:22:34 +0600

    added

---
+++ src/components/Home/Branch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Branch = () => {
+  return (
+    <div>
+      <h1>
+        this is mirpusr barnch mirpur branch is veery sensitive place other
+        branch is komlaour rallwin waty station is verry sensitibve palse there
+        are wonder full palse
+      </h1>
+    </div>
+  );
+};
+
+export default Branch;
