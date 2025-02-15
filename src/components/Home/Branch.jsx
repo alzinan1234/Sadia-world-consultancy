ddeed1ea052edc68b61b193cc839cc3dbcdab15a
johir-dev <108150533+johir-dev@users.noreply.github.com>
2025-02-15 21:08:27 +0600

    added

--- src/components/Home/Branch.jsx
+++ src/components/Home/Branch.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 
 const Branch = () => {
-  return (
-    <div>
-      <h1>
-        this is mirpusr barnch mirpur branch is veery sensitive place other
-        branch is komlaour rallwin waty station is verry sensitibve palse there
-        are wonder full palse
-      </h1>
-      <h1>
-        this is mirpusr barnch mirpur branch is veery sensitive place other
-        branch is komlaour rallwin waty station is verry sensitibve palse there
-        are wonder full palse
-      </h1>
-      <h1>
-        this is mirpusr barnch mirpur branch is veery sensitive place other
-        branch is komlaour rallwin waty station is verry sensitibve palse there
-        are wonder full palse
-      </h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Branch;
