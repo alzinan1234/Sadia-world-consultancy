5330bdd44b9a361c199e10bb84f95b4b45b8ad0c
johir-dev <pappurohan1100@gmail.com>
2025-02-20 20:47:02 +0600

    added

--- src/components/Home/Branch.jsx
+++ src/components/Home/Branch.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Branch = () => {
-  return (
-    <div>
-      <h1>this is framgate branch</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Branch;
