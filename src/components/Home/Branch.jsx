8b57acddc97c0955a2c195c0f5ecfe5df39bba00
johir-dev <pappurohan1100@gmail.com>
2025-02-20 20:50:45 +0600

    brach added

--- src/components/Home/Branch.jsx
+++ src/components/Home/Branch.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Branch = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        my name is zinan my father name is md mofazzal hossain my mother name is
+        farzana yesmin
+      </h1>
+    </div>
+  );
 };
 
 export default Branch;
