5c13d0dbca34cdac478a1b36465f62c4acb1bff0
johir-dev <pappurohan1100@gmail.com>
2025-03-13 19:41:55 +0600

    added

--- src/app/(site)/Visa/page.js
+++ src/app/(site)/Visa/page.js
@@ -3,8 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      {" "}
-      sadiya world is consulency for gose for help us us aus many country{" "}
+      
     </div>
   );
 };
